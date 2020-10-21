64948f95db2e0eeab656344756a5cd56b33f4063
Aidan Shannon <ajshannon@dmacc.edu>
2020-10-20 22:58:24 -0500

    test B

--- test/test_assign_average.py
+++ test/test_assign_average.py
@@ -6,6 +6,9 @@ class MyTestCase(unittest.TestCase):
     def test_A(self):
         self.assertEqual("You entered an A!", assign_average.switch_average('A'))
 
+    def test_B(self):
+        self.assertEqual("You entered an B!", assign_average.switch_average('B'))
+
 
 if __name__ == '__main__':
     unittest.main()
